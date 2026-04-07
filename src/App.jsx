8d13ce7efe7d10bb0d47518844be26187904037c
Gaalmeida-dev <gabrielalmeidaescalera@gmail.com>
2026-04-06 21:28:06 -0300

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blogs from "./pages/Blogs";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Navbar from "./components/Navbar";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<><Navbar/>, <Home/> </>
+  },
+  {
+    path: "/sobre",
+    element:<><Navbar/>, <About/> </>
+  },
+  {
+    path: "/blogs",
+    element:<><Navbar/>, <Blogs/> </>
+  },
+  {
+    path: "/login",
+    element:<><Navbar/>, <Login/> </>
+  },
+  {
+    path: "/logout",
+    element:<><Navbar/>, <Signup/> </>
+  },
+
+
+])
+
+const App = () =>{
+  return (
+    <>
+      <RouterProvider router ={router}/>
+    </>
+  )
+}
+
+export default App
