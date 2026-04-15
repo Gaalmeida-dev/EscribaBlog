3759b6042ec181ecb374b8ac9b4f6f9dca5226a4
Gaalmeida-dev <gabrielalmeidaescalera@gmail.com>
2026-04-15 02:41:22 -0300

    fix: corrige CORS para vercel

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: "https://escribablog-1.onrender.com",
+  origin: "https://escribablog.onrender.com",
   credentials: true,
 };
 app.use(cors(corsOptions));
